c0b1ae461c67d075c42aa3ab9819e63228331d5f
Parker Moore <parkrmoore@gmail.com>
2016-03-08 13:06:59 -0600

    jemoji: make it a hook

--- lib/jemoji.rb
+++ lib/jemoji.rb
@@ -3,39 +3,52 @@ require 'gemoji'
 require 'html/pipeline'
 
 module Jekyll
-  class Emoji < Jekyll::Generator
-    attr_reader :config
-    safe true
+  class Emoji
+    GITHUB_DOT_COM_ASSET_ROOT = "https://assets.github.com/images/icons/".freeze
 
-    def initialize(config = {})
-      @config = config
-    end
+    class << self
+      def emojify(doc)
+        src = emoji_src(doc.site.config)
+        doc.output = filter_with_emoji(src).call(doc.output)[:output].to_s
+      end
 
-    def src
-      @src ||=
+      # Public: Create or fetch the filter for the given {{src}} asset root.
+      #
+      # src - the asset root URL (e.g. https://assets.github.com/images/icons/)
+      #
+      # Returns an HTML::Pipeline instance for the given asset root.
+      def filter_with_emoji(src)
+        filters[src] ||= HTML::Pipeline.new([
+          HTML::Pipeline::EmojiFilter
+        ], { :asset_root => src })
+      end
+
+      # Public: Filters hash where the key is the asset root source.
+      # Effectively a cache.
+      def filters
+        @filters ||= {}
+      end
+
+      # Public: Calculate the asset root source for the given config.
+      # The custom emoji asset root can be defined in the config as
+      # emoji.src, and must be a valid URL (i.e. it must include a
+      # protocol and valid domain)
+      #
+      # config - the hash-like configuration of the document's site
+      #
+      # Returns a full URL to use as the asset root URL. Defaults to
+      # the assets.github.com emoji root.
+      def emoji_src(config = {})
         if config.key?("emoji") && config["emoji"].key?("src")
           config["emoji"]["src"]
         else
-          "https://assets.github.com/images/icons/"
+          GITHUB_DOT_COM_ASSET_ROOT
         end
-    end
-
-    def filter
-      @filter ||= HTML::Pipeline::EmojiFilter.new(nil, { :asset_root => src })
-    end
-
-    def generate(site)
-      site.posts.each { |doc| emojify doc } unless v3?
-      site.pages.each { |doc| emojify doc }
-      site.docs_to_write.each { |doc| emojify doc }
-    end
-
-    def emojify(page)
-      page.content = filter.emoji_image_filter(page.content)
-    end
-
-    def v3?
-      ::Jekyll::VERSION.to_f >= 3.0
+      end
     end
   end
+end
+
+Jekyll::Hooks.register [:pages, :documents], :post_render do |doc|
+  Jekyll::Emoji.emojify(doc) if doc.class == Jekyll::Page || doc.write?
 end
--- spec/jemoji_spec.rb
+++ spec/jemoji_spec.rb
@@ -1,62 +1,58 @@
 RSpec.describe(Jekyll::Emoji) do
+  Jekyll.logger.log_level = :error
+
   let(:config_overrides) { {} }
   let(:configs) do
     Jekyll.configuration(config_overrides.merge({
       'skip_config_files' => false,
-      'collections' => { 'docs' => { 'output' => true }, 'secret' => {} },
-      'source' => fixtures_dir,
-      'destination' => fixtures_dir('_site')
+      'collections'       => { 'docs' => { 'output' => true }, 'secret' => {} },
+      'source'            => fixtures_dir,
+      'destination'       => fixtures_dir('_site')
     }))
   end
-  let(:site) { Jekyll::Site.new(configs) }
-  let(:emoji) { site.generators.find { |g| g.class.name.eql?("Jekyll::Emoji") } }
+  let(:emoji)       { described_class }
+  let(:site)        { Jekyll::Site.new(configs) }
   let(:default_src) { "https://assets.github.com/images/icons/" }
-  let(:result) { "<img class='emoji' title=':+1:' alt=':+1:' src='#{default_src}emoji/unicode/1f44d.png' height='20' width='20' align='absmiddle' />" }
-  let(:v3?) { ::Jekyll::VERSION.to_f >= 3.0 }
-  let(:posts) { v3? ? site.posts.docs : site.posts }
+  let(:result)      { "<img class=\"emoji\" title=\":+1:\" alt=\":+1:\" src=\"#{default_src}emoji/unicode/1f44d.png\" height=\"20\" width=\"20\" align=\"absmiddle\">" }
+  let(:posts)       { site.posts.docs }
+
+  def para(content)
+    "<p>#{content}</p>\n"
+  end
 
   before(:each) do
     site.read
     (site.pages + posts + site.docs_to_write).each { |p| p.content.strip! }
-    site.generate
-  end
-
-  it "is instantiated properly with the site" do
-    expect(emoji).not_to be(nil)
-    expect(emoji).to be_a(Jekyll::Emoji)
+    site.render
   end
 
   it "creates a filter" do
-    expect(emoji.filter).to be_a(HTML::Pipeline::EmojiFilter)
+    expect(emoji.filters[default_src]).to be_a(HTML::Pipeline)
   end
 
   it "has a default source" do
-    expect(emoji.src).to eql(default_src)
-  end
-
-  it "saves the site config for later use" do
-    expect(emoji.config).to eql(site.config)
+    expect(emoji.emoji_src).to eql(default_src)
   end
 
   it "correctly replaces the emoji with the img in posts" do
-    expect(posts.first.content).to eql(result)
+    expect(posts.first.output).to eql(para(result))
   end
 
   it "correctly replaces the emoji with the img in pages" do
-    expect(site.pages.first.content).to eql(result)
+    expect(site.pages.first.output).to eql(para(result))
   end
 
   it "correctly replaces the emoji with the img in collection documents" do
-    expect(site.collections["docs"].docs.first.content).to eql(result)
+    expect(site.collections["docs"].docs.first.output).to eql(para(result))
   end
 
   it "does not replace the emoji if the collection document is not to be output" do
-    expect(site.collections["secret"].docs.first.content).to eql(":+1:\n")
+    expect(site.collections["secret"].docs.first.output).to eql(para(":+1:"))
   end
 
   it "does not mangle liquid templates" do
-    expect(site.collections["docs"].docs.last.content).to eql(
-      "#{result} <a href=\"{{ page.url }}\">{{ page.path }}</a>"
+    expect(site.collections["docs"].docs.last.output).to eql(
+      para("#{result} <a href=\"/docs/with_liquid.html\">_docs/with_liquid.md</a>")
     )
   end
 
@@ -69,11 +65,11 @@ RSpec.describe(Jekyll::Emoji) do
     end
 
     it "fetches the custom base from the config" do
-      expect(emoji.src).to eql(emoji_src)
+      expect(emoji.emoji_src(site.config)).to eql(emoji_src)
     end
 
     it "respects the new base when emojifying" do
-      expect(posts.first.content).to eql(result.sub(default_src, emoji_src))
+      expect(posts.first.output).to eql(para(result.sub(default_src, emoji_src)))
     end
   end
 end
