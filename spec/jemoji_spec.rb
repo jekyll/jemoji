31f3d88224975f537a839ce695160db61625b977
Ben Balter <ben.balter@github.com>
2017-04-03 10:21:54 -0400

    limit line length in spec

--- spec/jemoji_spec.rb
+++ spec/jemoji_spec.rb
@@ -15,7 +15,11 @@ RSpec.describe(Jekyll::Emoji) do
   let(:emoji)       { described_class }
   let(:site)        { Jekyll::Site.new(configs) }
   let(:default_src) { "https://assets-cdn.github.com/images/icons/" }
-  let(:result)      { "<img class=\"emoji\" title=\":+1:\" alt=\":+1:\" src=\"#{default_src}emoji/unicode/1f44d.png\" height=\"20\" width=\"20\" align=\"absmiddle\">" }
+  let(:result) do
+    result = "<img class=\"emoji\" title=\":+1:\" alt=\":+1:\" "
+    result << "src=\"#{default_src}emoji/unicode/1f44d.png\" "
+    result << "height=\"20\" width=\"20\" align=\"absmiddle\">"
+  end
 
   let(:posts)        { site.posts.docs }
   let(:basic_post)   { find_by_title(posts, "Refactor") }
